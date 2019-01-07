c0c9eee02cdde4a2fc266d4ad688e47cd75d98f4
Ricky zhao <rzhao3818@bths.edu>
2019-01-06 23:31:14 -0500

    It still doesn't work and I haven't figured out why

--- src/ActivateBubbleSort.java
+++ src/ActivateBubbleSort.java
@@ -1,7 +1,20 @@
 public class ActivateBubbleSort {
     public static void main(String[]args){
 
+        int[] arr = Sort.randIntArr(10);
 
+        for(int i = 0; i < arr.length;i++){
+            arr[i] = (int)(Math.random()*10001);
+        }
+        System.out.println("Initial Array");
+        for (int i = 0; i < arr.length;i++){
+            System.out.println(""+arr[i]+"");
+        }
 
+        BubbleSort.bubbleSort(arr);
+        System.out.println("Sorted Array");
+        for (int i = 0; i < arr.length;i++){
+            System.out.println(""+arr[i]+"");
+        }
     }
 }
