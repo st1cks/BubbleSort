dbd3358a2ff81960c8c615deb7b927210eabca20
BT_1N3_18 <rzhao3818@bths.edu>
2019-01-11 14:16:06 -0500

    it still doesnt work

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,17 +1,33 @@
 public class BubbleSort {
 
-    public static void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] sortthis) {
         boolean sortagain = false;
 
         while (sortagain ==true){
             sortagain = false;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    Sort.swap(arr, i, i + 1);
+            for (int i = 0; i < sortthis.length; i++) {
+                if (sortthis[i] > sortthis[i + 1]) {
+                    swap(sortthis, i, i + 1);
                     sortagain = true;
                 }
             }
 
         }
     }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int[] randIntArr(int count) {
+        int[] arr = new int[count];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10001);
+        }
+        return arr;
+    }
+
+
 }
